837ff15d35cedd4a0e392fbe842dcd9c8e0fb854
davidpilecek <davidpilecek@seznam.cz>
2022-11-01 20:34:19 +0100

    firs commit

---
+++ drive.py
@@ -0,0 +1,59 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import config as conf
+
+GPIO.setmode(GPIO.BOARD)
+
+class Robot():
+    def __init__(self, leftMot, rightMot):
+        GPIO.setwarnings(False)
+        self.leftMot = leftMot
+        self.rightMot = rightMot
+        GPIO.setup(leftMot,GPIO.OUT)
+        GPIO.setup(rightMot,GPIO.OUT)
+        self.pwmL = GPIO.PWM(self.leftMot,conf.frequency)
+        self.pwmL.start(0)
+        self.pwmR = GPIO.PWM(self.rightMot,conf.frequency)
+        self.pwmR.start(0)
+    def straight(self, speed):
+        self.pwmL.ChangeDutyCycle(speed)
+        self.pwmR.ChangeDutyCycle(speed)
+    def moveL(self, speed):
+        self.pwmL.ChangeDutyCycle(speed)
+        self.pwmR.ChangeDutyCycle(0)
+    def moveR(self, speed):
+        self.pwmL.ChangeDutyCycle(0)
+        self.pwmR.ChangeDutyCycle(speed)
+        
+    def stop(self,time = 0):
+        self.pwmL.ChangeDutyCycle(0)
+        self.pwmR.ChangeDutyCycle(0)
+        
+class Servo():
+    def __init__(self, servoPin):
+        GPIO.setwarnings(False)
+        self.servoPin = servoPin
+        GPIO.setup(self.servoPin, GPIO.OUT)
+        self.pwmServo = GPIO.PWM(self.servoPin, 50)
+        self.pwmServo.start(0)
+    def setAngle(self, servoAngle):
+        self.servoAngle = servoAngle
+        self.dutyCycle = self.servoAngle / 36 + 5
+        self.pwmServo.ChangeDutyCycle(self.dutyCycle)
+        sleep(1)
+        self.pwmServo.ChangeDutyCycle(0)
+        
+
+def test():
+    
+    robot = Robot(conf.leftMot, conf.rightMot)
+    
+    for i in range(100):
+        robot.move(i)
+        print(i)
+        sleep(0.1)
+    
+
+if __name__ == "__main__":
+    test()
+
