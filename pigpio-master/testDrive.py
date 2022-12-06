52a1aa07656b95957ae8ed70d8d826894c65e60b
David <pilecekd@gmail.com>
2022-12-06 20:17:49 +0000

    rpi

--- pigpio-master/testDrive.py
+++ pigpio-master/testDrive.py
@@ -21,17 +21,17 @@ image_draw = None
 
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
-T = conf.threshold
+
 frame_draw = []
 angle = 0
 ret = None
 cX, cY = [0, 0]
 index = 0
 last_time = 0
-
+try_line = True
 selection = conf.crop_selection
 line_found = 0
-dir = 0
+do_drive = False
 
 while True:
     currAngleX = servoX.getAngle()
@@ -47,48 +47,60 @@ while True:
         ret, area = cfu.crop_img_line_color(blurred, height, width, conf.blue, selection)
         mask_obj = cfu.obj_mask(blurred, conf.red)
 
-    try:
-        angle, image_draw = cfu.contours_line(frameOrig, ret, height, width)
-        line_found = 1
-
-    except Exception as e:
-        print("No line")
-        line_found = 0
+    if(try_line == False):
+        print("NOT TRYING LINE")
+        pass
+    else:
+        try:
+            angle, image_draw = cfu.contours_line(frameOrig, ret, height, width)
+            print("FOUND LINE")
+            line_found = 1
+            do_drive = True
+        except Exception as e:
+            print("No line")
+            line_found = 0
+            try_line = False
 
     if (line_found == 0):
-        robot.stop()
-        last_time = time.time()
-        servoY.setAngle(conf.servoY_pos)
-        if (dir == -1):
-            print("looking right")
-            servoX.setAngle(currAngleX + 20)
-        elif (dir == 1):
-            print("looking left")
-            servoX.setAngle(currAngleX - 20)
-        if(time.time() - last_time >= 5):
-            servoX.setAngle(conf.servoX_pos)
-            servoY.setAngle(conf.servoY_pos)
-            print("returned")
+         print("stopping bot")
+         do_drive = False
+         try_line = True
+
+#         last_time = time.time()
+#         servoY.setAngle(conf.servoY_pos)
+        #if (dir == -1):
+        #    print("looking right")
+       #     servoX.setAngle(currAngleX + 20)
+      #  elif (dir == 1):
+     #       print("looking left")
+    #        servoX.setAngle(currAngleX - 20)
+#         if(time.time() - last_time >= 5):
+#             servoX.setAngle(conf.servoX_pos)
+#             servoY.setAngle(conf.servoY_pos)
+#             print("returned")
 
     dev, dir = cfu.deviance(angle)
 
-    if dev + conf.basePwm > conf.pwmMax:
-        if dir == 1:
-            robot.moveL(conf.basePwm)
-        elif dir == -1:
-            robot.moveR(conf.basePwm)
+    if(do_drive == True):
+        if dev + conf.basePwm > conf.pwmMax:
+            if dir == 1:
+                robot.moveL(conf.basePwm)
+            elif dir == -1:
+                robot.moveR(conf.basePwm)
 
+        else:
+            last_direction = cfu.steer(conf.basePwm, dev, dir, robot)
     else:
-        last_direction = cfu.steer(conf.basePwm, dev, dir, robot)
-
+            robot.stop()
     try:
         cv.imshow("main", image_draw)
 
     except Exception as e:
-        print(str(e))    
-
+        print("no image to print")    
+        robot.stop()
     if cv.waitKey(1) == ord('q'):
         break
+
 robot.stop()    
 cap.release()
 cv.destroyAllWindows()
