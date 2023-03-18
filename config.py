b5de52a53d77af12e4d62df8e44727594f37745c
davidpilecek <78658446+davidpilecek@users.noreply.github.com>
2023-03-18 17:56:25 +0100

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ PATH_PIC_PI = r"/home/pi/Pramakon/pics"
 PATH_PIC_PC = r"C:\Users\david\Documents\Pramakon\pics"
 
 BLUE_HSV_RANGE = np.array([[95,55,35], [135,255,255]])
-GREEN_HSV_RANGE = np.array([[35, 50, 50], [90, 255, 255]])
+GREEN_HSV_RANGE = np.array([40, 50, 50], [80, 255, 255]])
 
 
 ANGLE_TOLERANCE = 10
